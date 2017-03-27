87686693048922fd006b4cd81420bc926e66a00c
alexey lashnev <a.lashnev@platron.ru>
2017-03-27 23:35:51 +0300

    Протестированые тесты

--- tests/request/clients/ClientTest.php
+++ tests/request/clients/ClientTest.php
@@ -4,6 +4,7 @@ namespace Platron\PhpSdk\tests;
 
 use Platron\PhpSdk\request\clients\PostClient;
 use Platron\PhpSdk\Exception;
+use Platron\PhpSdk\request\request_builders\RequestBuilder;
 
 class ClientTest extends \PHPUnit_Framework_TestCase {
 	
@@ -20,8 +21,9 @@ class ClientTest extends \PHPUnit_Framework_TestCase {
 	 * @param array $parameters
 	 */
 	public function testRequest($url, $parameters){
-		try {		
-			$this->fixture->request($url, $parameters);
+		try {
+			$requestBuilder = $this->generateMock($url, $parameters);
+			$this->fixture->request($requestBuilder);
 		}
 		catch (Exception $e){
 			return true;
@@ -30,11 +32,24 @@ class ClientTest extends \PHPUnit_Framework_TestCase {
 		return false;
 	}
 	
-	public function provider(){
+	public function provider(){	
 		return array(
 			array('www.not-found-site.sdcasdasdcasdc', array(1,2,3)),
 			array('www.platron.ru/init_payment.php', array(1,2,3)),
 			array('www.google.com', array()),
 		);
+	}
+	
+	/**
+	 * 
+	 * @param string $url
+	 * @param array $params
+	 * @return Platron\PhpSdk\request\request_builders\RequestBuilder
+	 */
+	protected function generateMock($url, $params){
+		$stubRequestBuilder = $this->getMockBuilder('Platron\PhpSdk\request\request_builders\RequestBuilder')->disableOriginalConstructor()->setMethods(array('getParameters','getRequestUrl'))->getMock();
+		$stubRequestBuilder->expects($this->any())->method('getParameters')->willReturn($params);
+		$stubRequestBuilder->expects($this->any())->method('getRequestUrl')->willReturn($url);
+		return $stubRequestBuilder;
 	}
 }
