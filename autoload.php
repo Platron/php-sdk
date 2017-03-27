b63e41e66e587634d166d45abf652136c7f2451a
alexey lashnev <a.lashnev@platron.ru>
2017-03-27 21:25:25 +0300

    Новый автолоад в связи с изменением папок

--- autoload.php
+++ autoload.php
@@ -8,10 +8,12 @@ class PlatronSdkAutoloader {
 	public static function autoload($className) {
 		$parsedPath = explode('\\', $className);
 		
-		if(empty($parsedPath) || $parsedPath[0] != 'platron_sdk' ){
+		if(empty($parsedPath) || $parsedPath[0] != 'Platron' ){
 			return;
 		}
 		unset($parsedPath[0]);
+		unset($parsedPath[1]);
+		array_unshift($parsedPath, 'src');
 
 		include(implode('\\',$parsedPath).'.php');
 	}
