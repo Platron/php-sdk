0704a054b150b538b506e12c53634d3c5f2ec307
alexey lashnev <a.lashnev@platron.ru>
2017-06-12 17:54:55 +0300

    2 new tests

---
+++ tests/integration/CancelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Platron\PhpSdk\tests\integration;
+
+use Platron\PhpSdk\request\clients\PostClient;
+use Platron\PhpSdk\request\request_builders\InitPaymentBuilder;
+use Platron\PhpSdk\request\request_builders\CancelBuilder;
+
+/**
+ * Интеграционный тест отмены транзакции
+ */
+class CancelTest extends IntegrationTestBase {
+	/** @var int */
+	protected $paymentId;
+	
+	/** @var PostClient */
+	protected $postClient;
+	
+	public function setUp() {
+		parent::setUp();
+		
+		$postClient = new PostClient($this->merchantId, $this->secretKey);
+		$this->postClient = $postClient;
+		
+		$initPaymentBuilder = new InitPaymentBuilder('10.00', 'test php sdk');
+		$this->paymentId = (int)$postClient->request($initPaymentBuilder)->pg_payment_id;
+	}
+	
+	public function testCancel(){
+		$getStatusBuilder = new CancelBuilder($this->paymentId);
+		$getStatusResponse = $this->postClient->request($getStatusBuilder);
+		$this->assertEquals('ok', $getStatusResponse->pg_status);
+	}
+}
---
+++ tests/integration/GetRegistryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Platron\PhpSdk\tests\integration;
+
+use Platron\PhpSdk\request\clients\PostClient;
+use Platron\PhpSdk\request\request_builders\GetRegistryBuilder;
+
+/**
+ * Интеграционный тест запроса реестра платежей
+ */
+class GetRegistryTest extends IntegrationTestBase {
+	
+	public function testGetRegistry(){
+		$postClient = new PostClient($this->merchantId, $this->secretKey);
+		$getStatusBuilder = new GetRegistryBuilder(new \DateTime('2017-01-01'));
+		$getStatusResponse = $postClient->request($getStatusBuilder);
+		$this->assertEquals('ok', $getStatusResponse->pg_status);
+	}
+}
